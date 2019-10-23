e235d0146864be6f2f679824481c98ff980c0a8f
virjar <virjar@virjar.com>
2019-10-23 23:44:32 +0800

    tcp粘包处理有误，多个报文同时回数可能导致崩溃

--- sekiro-lib/src/main/java/com/virjar/sekiro/netty/protocol/SekiroNatMessageDecoder.java
+++ sekiro-lib/src/main/java/com/virjar/sekiro/netty/protocol/SekiroNatMessageDecoder.java
@@ -17,50 +17,8 @@ public class SekiroNatMessageDecoder extends ByteToMessageDecoder {
     private static final int URI_LENGTH_SIZE = 1;
 
 
-    protected SekiroNatMessage decode(ChannelHandlerContext ctx, ByteBuf in2) throws Exception {
-        //  ByteBuf in = (ByteBuf) super.decode(ctx, in2);
-        ByteBuf in = in2;
-        if (in == null) {
-            return null;
-        }
-
-        if (in.readableBytes() < HEADER_SIZE) {
-            return null;
-        }
-
-//        int magic = in.readInt();
-//        if (magic != Constants.protocolMagic) {
-//            throw new IllegalStateException("invalid message");
-//        }
-
-        int frameLength = in.readInt();
-        if (in.readableBytes() < frameLength) {
-            return null;
-        }
-        SekiroNatMessage sekiroNatMessage = new SekiroNatMessage();
-        byte type = in.readByte();
-        long sn = in.readLong();
-
-        sekiroNatMessage.setSerialNumber(sn);
-
-        sekiroNatMessage.setType(type);
-
-        byte uriLength = in.readByte();
-        byte[] uriBytes = new byte[uriLength];
-        in.readBytes(uriBytes);
-        sekiroNatMessage.setExtra(new String(uriBytes));
-
-        byte[] data = new byte[frameLength - TYPE_SIZE - SERIAL_NUMBER_SIZE - URI_LENGTH_SIZE - uriLength];
-        in.readBytes(data);
-        sekiroNatMessage.setData(data);
-
-        in.release();
-
-        return sekiroNatMessage;
-    }
-
     @Override
-    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
+    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) {
         while (true) {
             if (in.readableBytes() < HEADER_SIZE) {
                 return;
