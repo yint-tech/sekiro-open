4b7cfb5c305efbb5fbaff0aff9cd68a16d7b9b7b
virjar <virjar@virjar.com>
2019-10-23 23:29:37 +0800

    粘包解包错误

--- sekiro-lib/src/main/java/com/virjar/sekiro/netty/protocol/SekiroNatMessageDecoder.java
+++ sekiro-lib/src/main/java/com/virjar/sekiro/netty/protocol/SekiroNatMessageDecoder.java
@@ -2,11 +2,14 @@ package com.virjar.sekiro.netty.protocol;
 
 import com.virjar.sekiro.Constants;
 
+import java.util.List;
+
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
 import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
 
-public class SekiroNatMessageDecoder extends LengthFieldBasedFrameDecoder {
+public class SekiroNatMessageDecoder extends ByteToMessageDecoder {
 
     private static final byte HEADER_SIZE = 4;
 
@@ -16,34 +19,10 @@ public class SekiroNatMessageDecoder extends LengthFieldBasedFrameDecoder {
 
     private static final int URI_LENGTH_SIZE = 1;
 
-    /**
-     * @param maxFrameLength
-     * @param lengthFieldOffset
-     * @param lengthFieldLength
-     * @param lengthAdjustment
-     * @param initialBytesToStrip
-     */
-    public SekiroNatMessageDecoder(int maxFrameLength, int lengthFieldOffset, int lengthFieldLength, int lengthAdjustment,
-                                   int initialBytesToStrip) {
-        super(maxFrameLength, lengthFieldOffset, lengthFieldLength, lengthAdjustment, initialBytesToStrip);
-    }
 
-    /**
-     * @param maxFrameLength
-     * @param lengthFieldOffset
-     * @param lengthFieldLength
-     * @param lengthAdjustment
-     * @param initialBytesToStrip
-     * @param failFast
-     */
-    public SekiroNatMessageDecoder(int maxFrameLength, int lengthFieldOffset, int lengthFieldLength, int lengthAdjustment,
-                                   int initialBytesToStrip, boolean failFast) {
-        super(maxFrameLength, lengthFieldOffset, lengthFieldLength, lengthAdjustment, initialBytesToStrip, failFast);
-    }
-
-    @Override
     protected SekiroNatMessage decode(ChannelHandlerContext ctx, ByteBuf in2) throws Exception {
-        ByteBuf in = (ByteBuf) super.decode(ctx, in2);
+        //  ByteBuf in = (ByteBuf) super.decode(ctx, in2);
+        ByteBuf in = in2;
         if (in == null) {
             return null;
         }
@@ -81,5 +60,39 @@ public class SekiroNatMessageDecoder extends LengthFieldBasedFrameDecoder {
         in.release();
 
         return sekiroNatMessage;
+    }
+
+    @Override
+    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
+        while (true) {
+            if (in.readableBytes() < HEADER_SIZE) {
+                return;
+            }
+
+            int frameLength = in.readInt();
+            if (in.readableBytes() < frameLength) {
+                return;
+            }
+            SekiroNatMessage sekiroNatMessage = new SekiroNatMessage();
+            byte type = in.readByte();
+            long sn = in.readLong();
+
+            sekiroNatMessage.setSerialNumber(sn);
+
+            sekiroNatMessage.setType(type);
+
+            byte uriLength = in.readByte();
+            byte[] uriBytes = new byte[uriLength];
+            in.readBytes(uriBytes);
+            sekiroNatMessage.setExtra(new String(uriBytes));
+
+            byte[] data = new byte[frameLength - TYPE_SIZE - SERIAL_NUMBER_SIZE - URI_LENGTH_SIZE - uriLength];
+            in.readBytes(data);
+            sekiroNatMessage.setData(data);
+
+            out.add(sekiroNatMessage);
+            // in.release();
+
+        }
     }
 }
