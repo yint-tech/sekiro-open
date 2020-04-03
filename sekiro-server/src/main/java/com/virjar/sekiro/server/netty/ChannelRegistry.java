ee6c602666f6337810fbd645d7ac70401a94fded
virjar <virjar@virjar.com>
2020-04-03 19:25:02 +0800

    队列操作全程加锁

--- sekiro-server/src/main/java/com/virjar/sekiro/server/netty/ChannelRegistry.java
+++ sekiro-server/src/main/java/com/virjar/sekiro/server/netty/ChannelRegistry.java
@@ -9,8 +9,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.BlockingDeque;
-import java.util.concurrent.LinkedBlockingDeque;
 
 import io.netty.channel.Channel;
 import lombok.extern.slf4j.Slf4j;
@@ -45,70 +43,8 @@ public class ChannelRegistry {
         }
     }
 
-    private static class ClientGroup {
-        public ClientGroup(String group) {
-            this.group = group;
-        }
 
-        private String group;
-        private Map<String, NatClient> natClientMap = Maps.newConcurrentMap();
-
-        private BlockingDeque<NatClient> poolQueue = new LinkedBlockingDeque<>();
-
-        public NatClient getByClientId(String clientId) {
-            NatClient ret = natClientMap.get(clientId);
-            if (ret != null && !ret.getCmdChannel().isActive()) {
-                natClientMap.remove(clientId);
-            }
-            return ret;
-        }
-
-        public NatClient allocateOne() {
-            while (true) {
-                NatClient poll = poolQueue.poll();
-                if (poll == null) {
-                    log.info("pool queue empty");
-                    return null;
-                }
-                if (!poll.getCmdChannel().isActive()) {
-                    //TODO queue 的数据结构不合理，需要支持线性remove
-                    NatClient realNatClient = natClientMap.get(poll.getClientId());
-                    if (realNatClient == poll) {
-                        log.info("remove channel for client:{}", poll.getClientId());
-                        natClientMap.remove(poll.getClientId());
-                    }
-                    continue;
-                }
-
-                poolQueue.add(poll);
-                return poll;
-            }
-
-        }
-
-
-        public synchronized void registryClient(String client, Channel cmdChannel) {
-
-            NatClient natClient = natClientMap.get(client);
-            if (natClient != null) {
-                Channel cmdChannelOld = natClient.getCmdChannel();
-                if (cmdChannelOld != cmdChannel) {
-                    log.info("old channel exist,attach again，oldChannel:{}  now channel:{} client:{}", cmdChannelOld, cmdChannel, client);
-                    natClient.attachChannel(cmdChannel);
-                }
-                return;
-            }
-            log.info("register a client :{} with channel:{} ", client, cmdChannel);
-            natClient = new NatClient(client, group, cmdChannel);
-            natClientMap.put(client, natClient);
-            poolQueue.add(natClient);
-        }
-
-
-    }
-
-
-    public synchronized void registryClient(String client, Channel cmdChannel) {
+    public void registryClient(String client, Channel cmdChannel) {
         log.info("register for client:{}", client);
 
         int index = client.indexOf("@");
@@ -158,7 +94,7 @@ public class ChannelRegistry {
         if (clientGroup == null) {
             return Collections.emptyList();
         }
-        Collection<NatClient> natClients = clientGroup.natClientMap.values();
+        Collection<NatClient> natClients = clientGroup.queue();
         List<String> clientVo = Lists.newArrayList();
         for (NatClient natClient : natClients) {
             if (natClient.getCmdChannel() != null && natClient.getCmdChannel().isActive()) {
---
+++ sekiro-server/src/main/java/com/virjar/sekiro/server/netty/ClientGroup.java
@@ -0,0 +1,104 @@
+package com.virjar.sekiro.server.netty;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import io.netty.channel.Channel;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+class ClientGroup {
+    ClientGroup(String group) {
+        this.group = group;
+    }
+
+    private String group;
+    private Map<String, NatClient> natClientMap = Maps.newConcurrentMap();
+
+    private LinkedList<String> poolQueue = new LinkedList<>();
+
+    //对象操作全部加锁，防止并发紊乱
+    synchronized List<NatClient> queue() {
+        List<NatClient> ret = Lists.newArrayListWithCapacity(poolQueue.size());
+        for (String key : poolQueue) {
+            NatClient natClient = natClientMap.get(key);
+            if (natClient == null) {
+                natClientMap.remove(key);
+                removeQueue(key);
+                continue;
+            }
+            Channel cmdChannel = natClient.getCmdChannel();
+            if (cmdChannel == null || !cmdChannel.isActive()) {
+                natClientMap.remove(key);
+                removeQueue(key);
+                continue;
+            }
+            ret.add(natClient);
+        }
+        return ret;
+    }
+
+    synchronized NatClient getByClientId(String clientId) {
+        NatClient ret = natClientMap.get(clientId);
+        if (ret == null) {
+            return null;
+        }
+        if (!ret.getCmdChannel().isActive()) {
+            natClientMap.remove(clientId);
+            removeQueue(clientId);
+        }
+        return ret;
+    }
+
+    synchronized NatClient allocateOne() {
+        while (true) {
+            String poll = poolQueue.poll();
+            if (poll == null) {
+                log.info("pool queue empty");
+                return null;
+            }
+
+            NatClient natClient = natClientMap.get(poll);
+            if (natClient == null) {
+                continue;
+            }
+            if (natClient.getCmdChannel() == null) {
+                natClientMap.remove(poll);
+                continue;
+            }
+            if (!natClient.getCmdChannel().isActive()) {
+                natClientMap.remove(poll);
+                continue;
+            }
+            poolQueue.add(poll);
+            return natClient;
+        }
+
+    }
+
+
+    synchronized void registryClient(String client, Channel cmdChannel) {
+        NatClient natClient = natClientMap.get(client);
+        if (natClient != null) {
+            Channel cmdChannelOld = natClient.getCmdChannel();
+            if (cmdChannelOld != cmdChannel) {
+                log.info("old channel exist,attach again，oldChannel:{}  now channel:{} client:{}", cmdChannelOld, cmdChannel, client);
+                natClient.attachChannel(cmdChannel);
+            }
+            return;
+        }
+        log.info("register a client :{} with channel:{} ", client, cmdChannel);
+        natClient = new NatClient(client, group, cmdChannel);
+        natClientMap.put(client, natClient);
+        removeQueue(client);
+        poolQueue.add(client);
+    }
+
+    private void removeQueue(String clientId) {
+        while (poolQueue.remove(clientId)) ;
+    }
+}
