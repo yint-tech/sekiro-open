5db229849693041073b42931c86c6d62cc763da0
virjar <virjar@virjar.com>
2021-06-12 14:15:39 +0800

    fix: http代码被错误删除

--- samples/src/main/java/com/virjar/sekiro/business/samples/DemoServerClient.java
+++ samples/src/main/java/com/virjar/sekiro/business/samples/DemoServerClient.java
@@ -1,16 +1,36 @@
 package com.virjar.sekiro.business.samples;
 
-import com.virjar.sekiro.business.api.ClusterSekiroClient;
 import com.virjar.sekiro.business.api.SekiroClient;
+import com.virjar.sekiro.business.api.interfaze.*;
 
 import java.util.UUID;
 
 public class DemoServerClient {
     public static void main(String[] args) {
-        // 使用集群客户端访问连接demo server ，不推荐。demo server没有使用集群客户端的必要。因为demo server都是单点的
-        ClusterSekiroClient demoServerClient = new ClusterSekiroClient("test-group", UUID.randomUUID().toString(), "http://sekiro.virjar.com:5620/business-demo/mock-allocate?ip=sekiro.virjar.com");
 
         // 通过指定服务器地址的方式连接到demo server，推荐这种方案。
-        SekiroClient sekiroClient = new SekiroClient("test", UUID.randomUUID().toString(), "sekiro.virjar.com", 5620);
+        SekiroClient sekiroClient = new SekiroClient("test_group", UUID.randomUUID().toString(), "127.0.0.1", 5620);
+
+
+        sekiroClient.setupSekiroRequestInitializer((sekiroRequest, handlerRegistry) -> handlerRegistry.registerSekiroHandler(new ActionHandler() {
+            @Override
+            public String action() {
+                return "test";
+            }
+
+            @Override
+            public void handleRequest(SekiroRequest sekiroRequest, SekiroResponse sekiroResponse) {
+
+                sekiroResponse.success("testParam:" + sekiroRequest.getString("testParam")
+                        + " not Time:" + System.currentTimeMillis());
+            }
+        })).start();
+
+        // 测试步骤：
+        // 1. 用idea或者AndroidStudio打开 本项目
+        // 2. 运行： sekiro-service-demo/src/main/java/com/virjar/sekiro/business/netty/Bootstrap.java
+        // 3. 运行： samples/src/main/java/com/virjar/sekiro/business/samples/DemoServerClient.java
+        // 4. 在浏览器中打开： http://127.0.0.1:5620/business-demo/invoke?group=test_group&action=test&testParam=%E5%8F%82%E6%95%B0%E4%BC%A0%E9%80%92%E6%B5%8B%E8%AF%95
+
     }
 }
